3e8bbd282bb5dfa61b3c095b967562a9220880eb
DESKTOP-33E0APO\admin <tourplan1@nate.com>
2024-09-23 15:47:18 +0900

    no message

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
 
 # Create your views here.
@@ -13,3 +13,19 @@ def notice(request):
 def notice_view(request,pk):
     notice = Notice.objects.get(pk=pk)
     return render(request, 'main/notice_view.html', {'notice':notice})
+
+def notice_add(request):
+    if request.method == 'POST':
+        new_notice = Notice.objects.create(
+            title = request.POST['title'],
+            contents = request.POST['contents'],
+            views = 0,
+        )
+        return redirect('/notice/')
+    return render(request, 'main/notice_add.html')
+
+def notice_remove(request,pk):
+    if request.method == 'POST':
+        notice = Notice.objects.get(pk=pk)
+        notice.delete()
+    return redirect('/notice/')
